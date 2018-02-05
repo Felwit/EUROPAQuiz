df6de07a66d69034879b366e13cc67b20aca9ac5
Felwit <36150087+Felwit@users.noreply.github.com>
2018-02-05 08:56:07 +0100

    Add files via upload

---
+++ Europaquiz Eingabe und Eingabeprügfung 20180129 - Kopie/Europaquiz Eingabe und Eingabeprügfung/Europaquiz-Hauptfenster.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Europaquiz_Eingabe_und_Eingabeprügfung
+{
+    partial class Europaquiz
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Button_prüfe_Land_neu = new System.Windows.Forms.Button();
+            this.tb_Land = new System.Windows.Forms.TextBox();
+            this.tb_Hauptstadt = new System.Windows.Forms.TextBox();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.SuspendLayout();
+            // 
+            // Button_prüfe_Land_neu
+            // 
+            this.Button_prüfe_Land_neu.Location = new System.Drawing.Point(447, 12);
+            this.Button_prüfe_Land_neu.Name = "Button_prüfe_Land_neu";
+            this.Button_prüfe_Land_neu.Size = new System.Drawing.Size(123, 20);
+            this.Button_prüfe_Land_neu.TabIndex = 0;
+            this.Button_prüfe_Land_neu.Text = "Nächstes Land";
+            this.Button_prüfe_Land_neu.UseVisualStyleBackColor = true;
+            this.Button_prüfe_Land_neu.Click += new System.EventHandler(this.Button_prüfe_Land_neu_Click);
+            // 
+            // tb_Land
+            // 
+            this.tb_Land.Location = new System.Drawing.Point(12, 12);
+            this.tb_Land.Name = "tb_Land";
+            this.tb_Land.Size = new System.Drawing.Size(208, 20);
+            this.tb_Land.TabIndex = 1;
+            this.tb_Land.Text = "Bitte geben sie das Land ein.";
+            // 
+            // tb_Hauptstadt
+            // 
+            this.tb_Hauptstadt.Location = new System.Drawing.Point(233, 12);
+            this.tb_Hauptstadt.Name = "tb_Hauptstadt";
+            this.tb_Hauptstadt.Size = new System.Drawing.Size(208, 20);
+            this.tb_Hauptstadt.TabIndex = 2;
+            this.tb_Hauptstadt.Text = "Bitte geben sie die Hauptsadt ein.";
+            // 
+            // Europaquiz
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 454);
+            this.Controls.Add(this.tb_Hauptstadt);
+            this.Controls.Add(this.tb_Land);
+            this.Controls.Add(this.Button_prüfe_Land_neu);
+            this.Name = "Europaquiz";
+            this.Text = "Europaquiz";
+            this.Load += new System.EventHandler(this.Europaquiz_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Button_prüfe_Land_neu;
+        private System.Windows.Forms.TextBox tb_Land;
+        private System.Windows.Forms.TextBox tb_Hauptstadt;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+    }
+}
+
---
+++ Europaquiz Eingabe und Eingabeprügfung 20180129 - Kopie/Europaquiz Eingabe und Eingabeprügfung/Europaquiz-Hauptfenster.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Europaquiz_Eingabe_und_Eingabeprügfung
+{
+    class Land
+    {
+        //Eigenschaften
+        public string Landname;
+        public string Hauptstadt;
+
+
+        public Land(string Landname, string Hauptstadt)
+        {
+            this.Landname = Landname;
+            this.Hauptstadt = Hauptstadt;
+        }
+
+        //Get & Set von Landname und Hauptstadt
+        public void setLandname(string Landname) { this.Landname = Landname; }
+        public string getLandname() { return this.Landname; }
+
+        public void setHauptstadt(string Hauptstadt) { this.Hauptstadt = Hauptstadt; }
+        public string getHauptstadt() { return this.Hauptstadt; }
+
+        Prüfeland(string Land, string Hauptstadt)
+        {
+            if(this.Landname==Land)
+            {
+                //färbe hellgrün
+                if(this.Hauptstadt==Hauptstadt)
+                {
+                    //färbe dunkelgrün
+                }
+            }
+            else
+            {
+                //färbe rot
+            }
+        }
+
+
+    }
+    public partial class Europaquiz : Form
+    {
+        Random random = new Random();
+        int anzLänder = 0;
+        int Auswahl;
+        int[] gewälte = new int[1];//max größe ergibt sich eigentlich aus Schwierigkeit
+        public Europaquiz()
+        {
+            InitializeComponent();
+        }
+
+        private void Europaquiz_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_prüfe_Land_neu_Click(object sender, EventArgs e)
+        {
+            bool i = false;//brake Variable
+            if (Button_prüfe_Land_neu.Text == "Neues Land")
+            {
+                Button_prüfe_Land_neu.Text = "Prüfe";
+                Auswahl = random.Next(1, anzLänder);
+                while (i == false)
+                {
+                    if (!gewälte.Contains(Auswahl))
+                    {
+                        //färbe gelb
+                        i = true;
+                    }
+                }
+            }
+
+            else
+            {
+                string Land = tb_Land.Text;
+                string HStadt = tb_Hauptstadt.Text;
+                Prüfeland(Land,tb_Hauptstadt);//methode der klasse Land
+            }
+
+
+
+
+
+
+
+            }
+
+        }
+    }
+}
